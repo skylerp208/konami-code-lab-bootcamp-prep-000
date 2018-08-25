fcddd572b944200f12f75ac8322765bb0b01bf53
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-08-25 16:43:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,3 +14,17 @@ const codes = [
 function init() {
   // your code here
 }
+
+let index = 0
+function konamiTest(e) {
+  let key = e.key
+  if (key === codes[index]) {
+    index++
+    if (index === codes.length) {
+      alert("you fucking did it bb")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+}
